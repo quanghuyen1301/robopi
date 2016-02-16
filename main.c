52b421ee40c40780b52d03e40a9ee385825b1c53
quanghuyen@live.com <quanghuyen@live.com>
2016-02-16 13:23:18 +0000

    push

--- main.c
+++ main.c
@@ -6,7 +6,76 @@
  
 #include <stdio.h>
 #include <wiringPi.h>
- 
+#include "getch.c"
+#define GPIO_MOTO_UP_1 2 //RIGHT
+#define GPIO_MOTO_DOWN_1 0 //RIGHT
+
+#define GPIO_MOTO_UP_2 1 //LEFT
+#define GPIO_MOTO_DOWN_2 3
+void __configOutput()
+
+{
+pinMode (GPIO_MOTO_UP_1, OUTPUT) ;
+pinMode (GPIO_MOTO_DOWN_1, OUTPUT) ;
+pinMode (GPIO_MOTO_UP_2, OUTPUT) ;
+pinMode (GPIO_MOTO_DOWN_2, OUTPUT) ;
+} 
+
+void __stop()
+{
+digitalWrite(GPIO_MOTO_UP_1,0);
+digitalWrite(GPIO_MOTO_DOWN_1,0);
+digitalWrite(GPIO_MOTO_UP_2,0);
+digitalWrite(GPIO_MOTO_DOWN_2,0);
+}
+void __up()
+{
+	__stop();
+	digitalWrite(GPIO_MOTO_UP_1,1);
+	digitalWrite(GPIO_MOTO_UP_2,1);
+}
+void __down()
+{
+        __stop();
+        digitalWrite(GPIO_MOTO_DOWN_1,1);
+        digitalWrite(GPIO_MOTO_DOWN_2,1);
+}
+void __left()
+{
+        __stop();
+        digitalWrite(GPIO_MOTO_UP_1,1);
+//        digitalWrite(GPIO_MOTO_UP_2,1);
+}
+void __right()
+{
+        __stop();
+//        digitalWrite(GPIO_MOTO_UP_1,1);
+        digitalWrite(GPIO_MOTO_UP_2,1);
+}
+void __getkey()
+{
+char key;
+printf("w up , s down ,a left,d right , e to exit\n:");
+while(1)
+{
+ 	key =  getch();
+
+	if(key == 'w')
+		__up();
+else if(key == 's')
+		__down();
+else if(key == 'd')
+                __right();
+else if(key == 'a')
+                __left();
+else if(key == ' ')
+                __stop();
+else if(key == 'e') {
+		__stop();
+		break;
+	}
+}
+}
 int main (void)
 {
   printf ("Raspberry Pi blink\n") ;
@@ -14,14 +83,8 @@ int main (void)
   if (wiringPiSetup () == -1)
     return 1 ;
  
-  pinMode (0, OUTPUT) ;         // aka BCM_GPIO pin 17
+  __configOutput();
  
-  for (;;)
-  {
-    digitalWrite (0, 1) ;       // On
-    delay (500) ;               // mS
-    digitalWrite (0, 0) ;       // Off
-    delay (500) ;
-  }
+__getkey();
   return 0 ;
 }
